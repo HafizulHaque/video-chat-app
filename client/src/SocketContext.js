f5c78be4069ceae75999e5e642e4c765a5e6d021
Hafizul Haque <hafizul.haque@reddotdigitalit.com>
2023-05-15 08:17:22 +0600

    url updated

--- client/src/SocketContext.js
+++ client/src/SocketContext.js
@@ -4,7 +4,7 @@ import Peer from 'simple-peer'
 
 const SocketContext = createContext();
 
-const socket = io('https://video-chat-app-kappa.vercel.app/')
+const socket = io('https://simsim-backend.onrender.com/')
 
 const ContextProvider = ({ children }) => {
 
